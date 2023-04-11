cc8a7dcde65be1a5d5af579c3481f92ac8f827f6
storm311 <stormfrederik@gmail.com>
2023-04-11 07:22:30 +0200

    small changes to collision and cleanup of code

--- Collision/src/main/java/dk/sdu/mmmi/cbse/CollisionDetection.java
+++ Collision/src/main/java/dk/sdu/mmmi/cbse/CollisionDetection.java
@@ -1,10 +1,8 @@
 package dk.sdu.mmmi.cbse;
 
-import com.badlogic.gdx.Gdx;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.data.entityparts.SpritePart;
@@ -14,16 +12,10 @@ import zombie.Zombie;
 import java.awt.*;
 
 public class CollisionDetection implements IEntityProcessingService{
-    private PositionPart ent;
-    private PositionPart colEnt;
-    private SpritePart entSprite;
-    private SpritePart colEntSprite;
 
     @Override
     public void process(GameData gameData, World world){
         // TODO find a way to get the entities classes without the for-loop
-        // TODO find out if i fucked with other dependencies
-        // This brings my pis i kog
         /*
         for (Entity entity : world.getEntities()){
             PositionPart ent1 = entity.getPart(PositionPart.class);
@@ -55,25 +47,24 @@ public class CollisionDetection implements IEntityProcessingService{
         /*
            This works, no clue why the others don't, but this solution would be extremely memory heavy
            Finds all players and zombies in entities check if they overlap
-           and if true sets player position to 200,200
         */
         for (Entity entity : world.getEntities(Player.class)){
             PositionPart ent1 = entity.getPart(PositionPart.class);
+            SpritePart ent = entity.getPart(SpritePart.class);
             for (Entity colEntity : world.getEntities(Zombie.class)){
                 if (collideWith(entity, colEntity)) {
-                    ent1.setY(200);
-                    ent1.setX(200);
+                    world.removeEntity(colEntity);
                 }
             }
         }
 
     }
 
-    public Boolean collideWith(Entity entity, Entity collisionEntity){
-        ent = entity.getPart(PositionPart.class);
-        colEnt = collisionEntity.getPart(PositionPart.class);
-        entSprite = entity.getPart(SpritePart.class);
-        colEntSprite = collisionEntity.getPart(SpritePart.class);
+    private Boolean collideWith(Entity entity, Entity collisionEntity){
+        PositionPart ent = entity.getPart(PositionPart.class);
+        PositionPart colEnt = collisionEntity.getPart(PositionPart.class);
+        SpritePart entSprite = entity.getPart(SpritePart.class);
+        SpritePart colEntSprite = collisionEntity.getPart(SpritePart.class);
 
         // Creates rectangles behind the sprites to control hitboxes
         // using the current pos of the PositionPart and the dimensions
@@ -81,15 +72,8 @@ public class CollisionDetection implements IEntityProcessingService{
         Rectangle hitBoxEnt = new Rectangle((int) ent.getX(),(int) ent.getY(), (int) entSprite.getSizeWidth(), (int) entSprite.getSizeHeight());
         Rectangle hitBoxColEnt = new Rectangle((int) colEnt.getX(),(int) colEnt.getY(), (int) colEntSprite.getSizeWidth(), (int) colEntSprite.getSizeHeight());
 
-        // Checks if the two rectangles overlaps
-        boolean overlapping = hitBoxEnt.intersects(hitBoxColEnt);
-
-        // return based on if they overlap or not
-        if (!overlapping){
-            return false;
-        } else {
-            return true;
-        }
+        // return based on if they overlap or not, true if they do, false if not
+        return hitBoxEnt.intersects(hitBoxColEnt);
 
         /*
         float horizontal;
