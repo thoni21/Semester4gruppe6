1cd2c3ad1e0a68f7b3f26190f77ecd1db1f33e58
storm311 <stormfrederik@gmail.com>
2023-04-28 12:35:41 +0200

    Added collision event between zombie and player

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/events/CollisionEvent.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/events/CollisionEvent.java
@@ -3,6 +3,8 @@ package dk.sdu.mmmi.cbse.common.events;
 import dk.sdu.mmmi.cbse.common.Types.EntityTypes;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 
 public class CollisionEvent extends Event {
     private Entity colEntity;
@@ -19,6 +21,22 @@ public class CollisionEvent extends Event {
             // TODO: Kill/Take damage from the player an enemy - Make sure they don't overlap.
             LifePart sourceLifePart = super.getSource().getPart(LifePart.class);
             sourceLifePart.setIsHit(true);
+
+            PositionPart playerPos = super.getSource().getPart(PositionPart.class);
+            PositionPart enemyPos = colEntity.getPart(PositionPart.class);
+
+            if(playerPos.getX() < enemyPos.getX()){ // right side player
+                enemyPos.setX(enemyPos.getX()+5);
+            }
+            if (playerPos.getX() > enemyPos.getX()){ // left side player
+                enemyPos.setX(enemyPos.getX()-5);
+            }
+            if (playerPos.getY() > enemyPos.getY()){ // bottom side player
+                enemyPos.setY(enemyPos.getY()-5);
+            }
+            if (playerPos.getY() < enemyPos.getY()) { // top side player
+                enemyPos.setY(enemyPos.getY()+5);
+            }
         }
     }
 }
