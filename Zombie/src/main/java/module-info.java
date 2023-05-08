0e04991a79cc82e2973b26331bf2d891efecccda
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2023-05-08 16:04:12 +0200

    Import fix

--- Zombie/src/main/java/module-info.java
+++ Zombie/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module Enemy {
     uses dk.sdu.mmmi.cbse.common.services.IArtificialIntelligenceService;
     exports zombie;
     requires Common;
-    requires AI;
     requires com.badlogic.gdx;
 
     provides IGamePluginService with zombie.ZombiePlugin;
