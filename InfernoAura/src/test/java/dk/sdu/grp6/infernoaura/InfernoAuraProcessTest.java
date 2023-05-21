d7f772ce1d431b700a803457bea103d6d7c81b7f
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2023-05-21 13:12:01 +0200

    GroupID fix

--- InfernoAura/src/test/java/dk/sdu/grp6/infernoaura/InfernoAuraProcessTest.java
+++ InfernoAura/src/test/java/dk/sdu/grp6/infernoaura/InfernoAuraProcessTest.java
@@ -11,7 +11,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
-import java.util.Timer;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
